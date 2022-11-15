92aea6254be91a7df88609d5e8b6dfff92f13c75
jayesh-jain252 <jayesh64674@gmail.com>
2022-11-15 16:46:32 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,11 @@ const sendOpenLink = require("./routes/sendOpenLink");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
